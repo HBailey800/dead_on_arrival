21fb23f7ca1018126060920991b0701e2162e43c
LEPruitt <131303605+LEPruitt@users.noreply.github.com>
2025-04-16 13:41:46 -0500

    Fixed load error

--- DeadOnArrival/coding_ii_bullet_hell_project_condensed_v2/assets/Scripts/mainNode.gd
+++ DeadOnArrival/coding_ii_bullet_hell_project_condensed_v2/assets/Scripts/mainNode.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 
 func on_game_over():
 	emit_signal("gameOver")
-	await get_tree().create_timer(0.1).timeout
-	get_tree().change_scene_to_file("res://assets//Scenes/game_over_screen.tscn")
+	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://assets//Scenes//game_over_screen.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
