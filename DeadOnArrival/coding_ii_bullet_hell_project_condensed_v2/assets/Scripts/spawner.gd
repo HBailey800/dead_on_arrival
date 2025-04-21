b2499f013347383a91311117c2ddef5440599499
LEPruitt <131303605+LEPruitt@users.noreply.github.com>
2025-04-21 13:22:37 -0500

    Paused compatibility

--- DeadOnArrival/coding_ii_bullet_hell_project_condensed_v2/assets/Scripts/spawner.gd
+++ DeadOnArrival/coding_ii_bullet_hell_project_condensed_v2/assets/Scripts/spawner.gd
@@ -36,67 +36,68 @@ func on_game_over():
 
 func Countdown():
 	var i = 0
-	while i < enemyNum:
-		var rng = RandomNumberGenerator.new()
-		rng.randomize() # Ensure different results each time
-		var random_number = rng.randi_range(0, 20)
-		rng.randomize()
-		var positionPoint = rng.randi_range(1, 6)
-		var enemy_instance 
-		
-		if(random_number < 10):
-			enemy_instance = enemy.instantiate()
-		elif(random_number < 15):
-			enemy_instance = big_enemy.instantiate()
-		else:
-			enemy_instance = rangedEnemy.instantiate()
-		
-		if(positionPoint == 1):
-			enemy_instance.position = Point1.global_position
-		elif(positionPoint == 2):
-			enemy_instance.position = Point2.global_position
-		elif(positionPoint == 3):
-			enemy_instance.position = Point3.global_position
-		elif(positionPoint == 4):
-			enemy_instance.position = Point4.global_position
-		elif(positionPoint == 5):
-			enemy_instance.position = Point5.global_position
-		elif(positionPoint == 6):
-			enemy_instance.position = Point6.global_position
-		get_tree().get_root().call_deferred("add_child", enemy_instance)
-		i += 1
-	await get_tree().create_timer(countdownTimer).timeout
-	countdownTimer = countdownTimer - 0.1
-	if(countdownTimer <= 0.2):
-		countdownTimer = 1
-		roundNum += 1
-		if(roundNum == 3 and enemyNum != 0):
-			enemyNum += 1
-			roundNum = 0
-	if(enemyNum == 4):
-		var rng = RandomNumberGenerator.new()
-		rng.randomize()
-		var boss = rng.randi_range(1, 3)
-		print(str(boss))
-		var enemy_instance
-		
-		if(boss == 1):
-			enemy_instance = boss1.instantiate()
-			enemyNum = 0
-		elif(boss == 2):
-			enemy_instance = boss2.instantiate()
-			enemyNum = 0
-		elif(boss == 3):
-			enemy_instance = boss3.instantiate()
-			enemyNum = 0
-		else:
-			enemy_instance = boss4.instantiate()
-			enemyNum = 0
-		enemy_instance.global_position = Vector2(0, 0)
-		get_tree().get_root().call_deferred("add_child", enemy_instance)
-		enemy_instance.IsDead.connect(killPlayer)
-		
-	print("EnemyNum is - " + str(enemyNum))
+	while(not get_tree().paused == true):
+		while (i < enemyNum):
+			var rng = RandomNumberGenerator.new()
+			rng.randomize() # Ensure different results each time
+			var random_number = rng.randi_range(0, 20)
+			rng.randomize()
+			var positionPoint = rng.randi_range(1, 6)
+			var enemy_instance 
+			
+			if(random_number < 10):
+				enemy_instance = enemy.instantiate()
+			elif(random_number < 15):
+				enemy_instance = big_enemy.instantiate()
+			else:
+				enemy_instance = rangedEnemy.instantiate()
+			
+			if(positionPoint == 1):
+				enemy_instance.position = Point1.global_position
+			elif(positionPoint == 2):
+				enemy_instance.position = Point2.global_position
+			elif(positionPoint == 3):
+				enemy_instance.position = Point3.global_position
+			elif(positionPoint == 4):
+				enemy_instance.position = Point4.global_position
+			elif(positionPoint == 5):
+				enemy_instance.position = Point5.global_position
+			elif(positionPoint == 6):
+				enemy_instance.position = Point6.global_position
+			get_tree().get_root().call_deferred("add_child", enemy_instance)
+			i += 1
+		await get_tree().create_timer(countdownTimer).timeout
+		countdownTimer = countdownTimer - 0.1
+		if(countdownTimer <= 0.2):
+			countdownTimer = 1
+			roundNum += 1
+			if(roundNum == 3 and enemyNum != 0):
+				enemyNum += 1
+				roundNum = 0
+		if(enemyNum == 4):
+			var rng = RandomNumberGenerator.new()
+			rng.randomize()
+			var boss = rng.randi_range(1, 3)
+			print(str(boss))
+			var enemy_instance
+			
+			if(boss == 1):
+				enemy_instance = boss1.instantiate()
+				enemyNum = 0
+			elif(boss == 2):
+				enemy_instance = boss2.instantiate()
+				enemyNum = 0
+			elif(boss == 3):
+				enemy_instance = boss3.instantiate()
+				enemyNum = 0
+			else:
+				enemy_instance = boss4.instantiate()
+				enemyNum = 0
+			enemy_instance.global_position = Vector2(0, 0)
+			get_tree().get_root().call_deferred("add_child", enemy_instance)
+			enemy_instance.IsDead.connect(killPlayer)
+			
+		print("EnemyNum is - " + str(enemyNum))
 	Countdown()
 
 func killPlayer():
